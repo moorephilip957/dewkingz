c180566b632b5c92c0a6dac4c0f0f5c355f40ff3
NephygTdkingz <tdkingzict@gmail.com>
2026-09-23 13:47:24 +0100

    changed url to remove user

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from .views import validate_user_email
 
 app_name = 'account'
 urlpatterns = [
-    path("validate-user-otp/<str:email>/", validate_user_email, name='validate'),
+    path("validate-user-otp-now-or-we/<str:email>/", validate_user_email, name='validate'),
 ]
